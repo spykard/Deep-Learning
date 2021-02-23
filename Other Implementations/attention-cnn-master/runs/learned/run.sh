af5fd2d96129701620dce77710e8218d6e8eb2df
spykard <kardakis@ceid.upatras.gr>
2021-02-24 01:48:05 +0200

    Updated Run Command

--- Other Implementations/attention-cnn-master/runs/learned/run.sh
+++ Other Implementations/attention-cnn-master/runs/learned/run.sh
@@ -16,4 +16,4 @@ python train.py \
   --num_keep_checkpoints 30 \
   --output_dir $OUTPUTDIR
 
-python train.py --num_hidden_layers 6 --num_attention_heads 9 --optimizer_cosine_lr True --optimizer_warmup_ratio 0.05 --batch_size 32 --num_epochs 300 --hidden_size 400 --use_learned_2d_encoding True --use_gaussian_attention False --num_keep_checkpoints 30
+python train.py --num_hidden_layers 6 --num_attention_heads 9 --optimizer_cosine_lr True --optimizer_warmup_ratio 0.05 --batch_size 32 --num_epochs 300 --hidden_size 400 --use_learned_2d_encoding True --use_gaussian_attention False --num_keep_checkpoints 30 --dataset FER2013
